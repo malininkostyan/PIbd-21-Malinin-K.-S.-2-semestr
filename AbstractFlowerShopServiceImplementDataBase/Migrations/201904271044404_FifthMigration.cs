944227144fb4f2331bcea42f0225713684c479b0
kostyan.malinin <kostyan.malinin@yandex.ru>
2019-04-27 14:51:39 +0400

    Lab_7 Malinin

---
+++ AbstractFlowerShopServiceImplementDataBase/Migrations/201904271044404_FifthMigration.cs
@@ -0,0 +1,37 @@
+namespace AbstractFlowerShopServiceImplementDataBase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FifthMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.InfoMessages",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        MessageId = c.String(),
+                        FromMailAddress = c.String(),
+                        Subject = c.String(),
+                        Body = c.String(),
+                        DeliveryDate = c.DateTime(nullable: false),
+                        CustomerId = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Customers", t => t.CustomerId)
+                .Index(t => t.CustomerId);
+            
+            AddColumn("dbo.Customers", "Mail", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.InfoMessages", "CustomerId", "dbo.Customers");
+            DropIndex("dbo.InfoMessages", new[] { "CustomerId" });
+            DropColumn("dbo.Customers", "Mail");
+            DropTable("dbo.InfoMessages");
+        }
+    }
+}
